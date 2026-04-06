eaaf75fe88d53d02f1a4c0cacacac07f018b3ec2
Jude Lopez <judecl@bu.edu>
2026-04-06 02:46:46 -0400

    sidebar update and browser location

--- ToolPool/ToolPool.Client/Program.cs
+++ ToolPool/ToolPool.Client/Program.cs
@@ -4,14 +4,19 @@ using ToolPool.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+//get localhost
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
+//blazor google maps
 var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 string googleMapsKey = await httpClient.GetStringAsync("/api/getSecret/GoogleMapsApiKey");
 builder.Services.AddBlazorGoogleMaps(googleMapsKey);
+
+//blazor bootstrap
+builder.Services.AddBlazorBootstrap();
 
 builder.Services.AddSingleton<CartService>();
 builder.Services.AddScoped<DemoItemService>();
